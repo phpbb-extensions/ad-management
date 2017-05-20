4098dbe0f4a065284d8d12b49beaad7be72dac2e
Jakub Senko <senky@users.noreply.github.com>
2017-05-20 10:59:06 +0200

    Update travis to test coverage

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -11,14 +11,12 @@
 set -e
 set -x
 
-EXTNAME=$1
-BRANCH=$2
-EXTPATH_TEMP=$3
+EPV=$1
+NOTESTS=$2
 
-# Copy extension to a temp folder
-mkdir ../../tmp
-cp -R . ../../tmp
-cd ../../
-
-# Clone phpBB
-git clone --depth=1 "git://github.com/phpbb/phpbb.git" "phpBB3" --branch=$BRANCH
+if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
+then
+	cd phpBB
+	composer require phpbb/epv:dev-master --dev --no-interaction
+	cd ../
+fi
