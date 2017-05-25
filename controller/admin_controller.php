df44739fb74f1517f2a9e779eb21610133ed5721
Jakub Senko <jakubsenko@gmail.com>
2017-05-25 21:03:10 +0200

    Define  in admin controller

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -35,20 +35,24 @@ class admin_controller
 	/** @var string php_ext */
 	protected $php_ext;
 
+	/** @var string phpbb_admin_path */
+	protected $phpbb_admin_path;
+
 	/** @var string Custom form action */
 	protected $u_action;
 
 	/**
 	* Constructor
 	*
-	* @param \phpbb\db\driver\driver_interface	$db			DB driver interface
-	* @param \phpbb\template\template			$template	Template object
-	* @param \phpbb\user						$user		User object
-	* @param \phpbb\request\request				$request	Request object
-	* @param string								$ads_table	Ads table
-	* @param string								$php_ext	PHP extension
+	* @param \phpbb\db\driver\driver_interface	$db					DB driver interface
+	* @param \phpbb\template\template			$template			Template object
+	* @param \phpbb\user						$user				User object
+	* @param \phpbb\request\request				$request			Request object
+	* @param string								$ads_table			Ads table
+	* @param string								$php_ext			PHP extension
+	* @param string								$phpbb_admin_path	Path to admin
 	*/
-	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\template\template $template, \phpbb\user $user, \phpbb\request\request $request, $ads_table, $php_ext)
+	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\template\template $template, \phpbb\user $user, \phpbb\request\request $request, $ads_table, $php_ext, $phpbb_admin_path)
 	{
 		$this->db = $db;
 		$this->template = $template;
@@ -56,6 +60,7 @@ class admin_controller
 		$this->request = $request;
 		$this->ads_table = $ads_table;
 		$this->php_ext = $php_ext;
+		$this->phpbb_admin_path = $phpbb_admin_path;
 	}
 
 	/**
@@ -341,7 +346,7 @@ class admin_controller
 		// Set output vars for display in the template
 		$this->template->assign_vars(array(
 			'U_ACTION_ADD'	=> $this->u_action . '&amp;action=add',
-			'ICON_PREVIEW'	=> '<img src="' . htmlspecialchars($phpbb_admin_path) . 'images/file_up_to_date.gif" alt="' . $this->user->lang('AD_PREVIEW') . '" title="' . $this->user->lang('AD_PREVIEW') . '" />',
+			'ICON_PREVIEW'	=> '<img src="' . htmlspecialchars($this->phpbb_admin_path) . 'images/file_up_to_date.gif" alt="' . $this->user->lang('AD_PREVIEW') . '" title="' . $this->user->lang('AD_PREVIEW') . '" />',
 		));
 	}
 }
