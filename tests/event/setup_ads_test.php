e6d25e83110c064efa2d961bc060f8ed65452614
Matt Friedman <maf675@gmail.com>
2017-06-12 18:30:32 -0700

    Improve setup_ads test

--- tests/event/setup_ads_test.php
+++ tests/event/setup_ads_test.php
@@ -30,6 +30,8 @@ class setup_ads_test extends main_listener_base
 		return array(
 			array(array(1)),
 			array(array(2)),
+			array(array(1, 2)),
+			array(array(2, 3)),
 		);
 	}
 
@@ -41,20 +43,23 @@ class setup_ads_test extends main_listener_base
 	public function test_setup_ads($hide_groups)
 	{
 		$this->user->data['user_id'] = 1;
+		$user_groups = $this->manager->load_memberships($this->user->data['user_id']);
 
 		$this->config_text->expects($this->once())
 			->method('get')
 			->with('phpbb_admanagement_hide_groups')
 			->willReturn(json_encode($hide_groups));
 
-		if ($hide_groups == array(2))
+		$ads = array();
+		if (count(array_intersect($hide_groups, $user_groups)) === 0)
 		{
 			$location_ids = $this->location_manager->get_all_location_ids();
 			$ads = $this->manager->get_ads($location_ids);
-
-			$this->template->expects($this->exactly(count($ads)))
-				->method('assign_vars');
 		}
+
+		$this->template
+			->expects($this->exactly(count($ads)))
+			->method('assign_vars');
 
 		$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
 		$dispatcher->addListener('core.page_header_after', array($this->get_listener(), 'setup_ads'));
