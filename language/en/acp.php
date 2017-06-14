24a3bdbecad4aa434a629f0bf1b795c095bd88d2
Matt Friedman <maf675@gmail.com>
2017-06-13 23:21:33 -0700

    Language updates

--- language/en/acp.php
+++ language/en/acp.php
@@ -20,23 +20,23 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'AD_NAME'				=> 'Name',
-	'AD_NAME_EXPLAIN'		=> 'This is only used for your recognition of this advertisement.',
+	'AD_NAME_EXPLAIN'		=> 'The ad name is only used to help you identify this advertisement.',
 	'AD_ENABLED'			=> 'Enabled',
 	'AD_ENABLED_EXPLAIN'	=> 'If disabled, this advertisement will not be displayed.',
 	'AD_END_DATE'			=> 'End Date',
-	'AD_END_DATE_EXPLAIN'	=> 'Set the date the advertisement will expire and become disabled. Leave this field blank if you do not want the advertisement to expire. Please, use <samp>YYYY-MM-DD</samp> format.',
+	'AD_END_DATE_EXPLAIN'	=> 'Set the date the advertisement will expire and become disabled. Leave this field blank if you do not want the advertisement to expire. Please use <samp>YYYY-MM-DD</samp> format.',
 	'AD_NOTE'				=> 'Notes',
 	'AD_NOTE_EXPLAIN'		=> 'Enter any notes for this advertisement. These notes are not shown anywhere except in the ACP.',
 	'AD_CODE'				=> 'Code',
-	'AD_CODE_EXPLAIN'		=> 'The advertisement code goes here. All code should be put in a raw HTML form, BBcodes are not supported.',
+	'AD_CODE_EXPLAIN'		=> 'Enter the advertisement code here. All code must use HTML markup, BBCodes are not supported.',
 	'AD_LOCATIONS'			=> 'Locations',
-	'AD_LOCATIONS_EXPLAIN'	=> 'Select locations to display this advertisement in. In case multiple ads are assigned the same location, only a random one will be displayed at a time. Mouse over the location to see further description.',
+	'AD_LOCATIONS_EXPLAIN'	=> 'Select locations where you want this advertisement displayed. Mouse over a location for a short description of it. If multiple ads use the same location, one ad will be randomly selected to display in that location each time. Use CTRL+CLICK (or CMD+CLICK on Mac) to select/deselect more than one location.',
 	'AD_ENABLE_TITLE'		=> array( // Plural rule doesn't apply here! Just translate the values.
 		0 => 'Click to enable',
 		1 => 'Click to disable',
 	),
 	'AD_EXPIRED_EXPLAIN'	=> 'This advertisement has expired and has been disabled.',
-	'ACP_ADS_EMPTY'			=> 'No advertisement, yet. Add one with the button below.',
+	'ACP_ADS_EMPTY'			=> 'No advertisements to display. Add one using the button below.',
 	'ACP_ADS_ADD'			=> 'Add new advertisement',
 	'ACP_ADS_EDIT'			=> 'Edit advertisement',
 	'AD_PREVIEW'			=> 'Preview this advertisement',
@@ -44,15 +44,15 @@ $lang = array_merge($lang, array(
 	'AD_NAME_REQUIRED'			=> 'Name is required.',
 	'AD_NAME_TOO_LONG'			=> 'Name length is limited to %d characters.',
 	'AD_END_DATE_INVALID'		=> 'The end date was invalid or has already expired.',
-	'ACP_AD_DOES_NOT_EXIST'		=> 'The advertisement does not exist!',
-	'ACP_AD_ADD_SUCCESS'		=> 'Advertisement added successfully!',
-	'ACP_AD_EDIT_SUCCESS'		=> 'Advertisement edited successfully!',
-	'ACP_AD_DELETE_SUCCESS'		=> 'Advertisement deleted successfully!',
-	'ACP_AD_DELETE_ERRORED'		=> 'There was an error deleting the advertisement!',
-	'ACP_AD_ENABLE_SUCCESS'		=> 'Advertisement enabled successfully!',
-	'ACP_AD_ENABLE_ERRORED'		=> 'There was an error enabling the advertisement!',
-	'ACP_AD_DISABLE_SUCCESS'	=> 'Advertisement disabled successfully!',
-	'ACP_AD_DISABLE_ERRORED'	=> 'There was an error disabling the advertisement!',
+	'ACP_AD_DOES_NOT_EXIST'		=> 'The advertisement does not exist.',
+	'ACP_AD_ADD_SUCCESS'		=> 'Advertisement added successfully.',
+	'ACP_AD_EDIT_SUCCESS'		=> 'Advertisement edited successfully.',
+	'ACP_AD_DELETE_SUCCESS'		=> 'Advertisement deleted successfully.',
+	'ACP_AD_DELETE_ERRORED'		=> 'There was an error deleting the advertisement.',
+	'ACP_AD_ENABLE_SUCCESS'		=> 'Advertisement enabled successfully.',
+	'ACP_AD_ENABLE_ERRORED'		=> 'There was an error enabling the advertisement.',
+	'ACP_AD_DISABLE_SUCCESS'	=> 'Advertisement disabled successfully.',
+	'ACP_AD_DISABLE_ERRORED'	=> 'There was an error disabling the advertisement.',
 
 	// Template locations
 	'AD_ABOVE_HEADER'				=> 'Above header',
@@ -80,5 +80,5 @@ $lang = array_merge($lang, array(
 	'HIDE_GROUPS'			=> 'Hide advertisements for groups',
 	'HIDE_GROUPS_EXPLAIN'	=> 'Members of selected groups will not see any advertisement.',
 
-	'ACP_AD_SETTINGS_SAVED'	=> 'Advertisement management settings has been saved.'
+	'ACP_AD_SETTINGS_SAVED'	=> 'Advertisement management settings saved.'
 ));
