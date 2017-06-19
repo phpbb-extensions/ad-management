7be4d9cef2e81b5bf13936bc3d735b1c88ec13f0
Jakub Senko <jakubsenko@gmail.com>
2017-06-19 10:48:05 +0200

    Adblocker -> Ad blocker

--- language/en/common.php
+++ language/en/common.php
@@ -19,6 +19,6 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ADBLOCKER_TITLE'	=> 'Adblocker detected',
+	'ADBLOCKER_TITLE'	=> 'Ad blocker detected',
 	'ADBLOCKER_MESSAGE'	=> 'Our website is made possible by displaying online advertisements to our visitors. Please consider supporting us by disabling your ad blocker on our website.',
 ));
