c4c3e176cbff409602fdf157988d95604327ef4f
Jakub Senko <jakubsenko@gmail.com>
2017-07-11 17:21:18 +0200

    Ensure correct zero is replaced in JS

--- styles/all/template/js/clicks.js
+++ styles/all/template/js/clicks.js
@@ -2,6 +2,6 @@ $(function() {
 	'use strict';
 
 	$('[data-ads-click]').on('click', function(e) {
-		$.get(u_phpbb_ads_click.replace('0', $(this).attr('data-ads-click')));
+		$.get(u_phpbb_ads_click.replace(/0$/, $(this).attr('data-ads-click')));
 	});
 });
