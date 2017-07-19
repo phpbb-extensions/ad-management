9117dbb03efa0544d9c90d4b0617e1d5591d70c0
Jakub Senko <jakubsenko@gmail.com>
2017-07-19 09:40:16 +0200

    Do not explain why views and/or clicks are hidden

--- language/en/ucp.php
+++ language/en/ucp.php
@@ -23,7 +23,4 @@ $lang = array_merge($lang, array(
 	'AD_VIEWS'	=> 'Views',
 	'AD_CLICKS'	=> 'Clicks',
 	'NO_ADS'	=> 'No advertisements to display. When you become an owner of one, it will immediately display its statistics here.',
-
-	'AD_VIEWS_DISABLED'		=> 'Advertisement views are not counted on this board, so they are not display in the table.',
-	'AD_CLICKS_DISABLED'	=> 'Advertisement clicks are not counted on this board, so they are not display in the table.',
 ));
