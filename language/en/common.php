0c607dbac0b44cd188b8712804a28a4a4e6d16a8
Jakub Senko <jakubsenko@gmail.com>
2017-08-02 18:30:00 +0200

    Make lang key more general

--- language/en/common.php
+++ language/en/common.php
@@ -22,5 +22,5 @@ $lang = array_merge($lang, array(
 	'ADBLOCKER_TITLE'	=> 'Ad blocker detected',
 	'ADBLOCKER_MESSAGE'	=> 'Our website is made possible by displaying online advertisements to our visitors. Please consider supporting us by disabling your ad blocker on our website.',
 
-	'POP_UP_TITLE'		=> 'ADVERTISEMENT',
+	'ADVERTISEMENT'		=> 'Advertisement',
 ));
