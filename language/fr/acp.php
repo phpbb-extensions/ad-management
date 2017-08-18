d65aef98e9fd85846a96738f2e70b9f4087d58d2
Raphaël M <Galixte@users.noreply.github.com>
2017-08-18 13:10:02 +0200

    Update the FRENCH language for the Advertisement Management extension

--- language/fr/acp.php
+++ language/fr/acp.php
@@ -41,6 +41,9 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	// Manage ads
 	'AD_SETTINGS'				=> 'Paramètres',
+	'ACTIVE_ADS'				=> 'Publicité activée',
+	'EXPIRED_ADS'				=> 'Publicité désactivée',
+	'STATUS'					=> 'Statut',
 	'AD_NAME'					=> 'Nom',
 	'AD_NAME_EXPLAIN'			=> 'Permet de saisir un nom pour identifier cette publicité.',
 	'AD_ENABLED'				=> 'Activation',
@@ -49,6 +52,8 @@ $lang = array_merge($lang, array(
 	'AD_NOTE_EXPLAIN'			=> 'Permet de saisir des annotations à propos de cette publicité. Ces notes sont optionnelles et ne seront pas affichées à l’exception de la gestion des publicités dans l’administration du forum.',
 	'AD_CODE'					=> 'Code',
 	'AD_CODE_EXPLAIN'			=> 'Permet de saisir le code de cette publicité. Le code doit utiliser des balises HTML, les BBCodes ne sont pas pris en charge.',
+	'ANALYSE_AD_CODE'			=> 'Analyser le code de la publicité',
+	'EVERYTHING_OK'				=> 'Le code apparait comme conforme.',
 	'AD_BANNER'					=> 'Bannière de la publicité',
 	'BANNER'					=> 'Transférer une bannière',
 	'BANNER_EXPLAIN'			=> 'Permet d’envoyer un fichier image au format JPG, GIF ou PNG. L’image sera sauvegardée dans le répertoire <samp>images</samp> de phpBB et une balise HTML IMG pour l’image sera automatiquement insérée dans le champ du code publicitaire.',
@@ -100,6 +105,12 @@ $lang = array_merge($lang, array(
 	'ACP_AD_DISABLE_SUCCESS'	=> 'Publicité désactivée avec succès.',
 	'ACP_AD_DISABLE_ERRORED'	=> 'Il y a eu une erreur durant la désactivation de cette publicité.',
 
+	// Analyser tests
+	'UNSECURE_CONNECTION'	=> '<strong>Contenu mixte</strong><br />Le forum fonctionne depuis une connexion HTTPS sécurisée, cependant le code de la publicité tente de charger du contenu depuis une connexion HTTP non sécurisée. La conséquence de cette situation pour les navigateurs Web est l’affichage d’avertissements à propos de contenus mixtes aux utilisateurs du forum les informant de la présence de contenus non sécurisés sur le forum.',
+	'SCRIPT_WITHOUT_ASYNC'	=> '<strong>Code Javascript asynchrone</strong><br />Le code cde cette publicité charge du code JavaScript de manière asynchrone. Cela signifie qu’il bloquera tout autre code Javascript jusqu’à son chargement complet, ayant pour conséquence de dégrader les performances. L’utilisation de l’attribut <samp>async</samp> permet d’améliorer la chargement de la page.',
+	'ALERT_USAGE'			=> '<strong>Utilisation de <samp>alert()</samp></strong><br />Le code utilise la fonction <samp>alert()</samp> ce qui n’est pas une pratique conforme et peut distraire les utilisateurs. Certains navigateurs Web peuvent bloquer le chargement de la page et afficher des avertissements aux utilisateurs.',
+	'LOCATION_CHANGE'		=> '<strong>Redirection</strong><br />Le code semble rediriger l’utilisateur vers une autre page ou site Web. Les redirections envoient les utilisateurs vers des destinations parfois hasardeuses, voire souvent malveillantes. Merci de s’assurer de l’intégrité de la destination de la redirection dans le code de la publicité.',
+
 	// Template locations
 	'AD_ABOVE_HEADER'				=> 'Au-dessus de l’entête',
 	'AD_ABOVE_HEADER_DESC'			=> 'Permet d’afficher la publicité sur chaque page au-dessus de l’entête du forum.',
@@ -126,9 +137,9 @@ $lang = array_merge($lang, array(
 	'AD_AFTER_FOOTER_NAVBAR'		=> 'En dessous de la barre de navigation du pied de page',
 	'AD_AFTER_FOOTER_NAVBAR_DESC'	=> 'Permet d’afficher la publicité sur chaque page en dessous de la barre de navigation du pied de page du forum.',
 	'AD_POP_UP'						=> 'Pop-up',
-	'AD_POP_UP_DESC'				=> 'Permet d’afficher la publicité un fois page jour en superposition du forum. L’utilisateur devra ferme la fenêtre pour continuer sa visite du forum. Merci de considérer que ce type d’affichage est intrusif pour l’utilisateur !',
+	'AD_POP_UP_DESC'				=> 'Permet d’afficher la publicité un fois par jour en superposition du forum. L’utilisateur devra fermer la fenêtre pour continuer sa visite du forum. Merci de considérer que ce type d’affichage est intrusif pour l’utilisateur !',
 	'AD_SLIDE_UP'					=> 'Défilement vers le haut',
-	'AD_SLIDE_UP_DESC'				=> 'Permet d’afficher la publicité sur chaque page lorsque l’utilisateur fait défiler la page vers le bas. La publicité défile vers le haut en partant du coin inférieur droit de la fenêtre.',
+	'AD_SLIDE_UP_DESC'				=> 'Permet d’afficher la publicité sur chaque page lorsque l’utilisateur fait défiler la page vers le bas. La publicité défile vers le haut en partant du bas de la page, au centre.',
 
 	// Settings
 	'ADBLOCKER_LEGEND'				=> 'Bloqueurs de publicités',
