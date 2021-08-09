e7b30a5efdfcafd013a1ad88dfa12988ce20e672
Matt Friedman <maf675@gmail.com>
2021-08-09 15:14:00 -0700

    Remove test code

--- styles/all/template/js/clicks.js
+++ styles/all/template/js/clicks.js
@@ -3,9 +3,7 @@
 
 	$(function() {
 		$('[data-phpbb-ads-id]').on('click', 'a', function(e) {
-			var url = u_phpbb_ads_click.replace(/(?:0\?sid=.+|0)$/, $(e.delegateTarget).attr('data-phpbb-ads-id'));
-			console.log(url);
-			$.get(url);
+			$.get(u_phpbb_ads_click.replace(/(?:0\?sid=.+|0)$/, $(e.delegateTarget).attr('data-phpbb-ads-id')));
 		});
 	});
 })(jQuery, u_phpbb_ads_click);
